542401640bfdd7f4cde36a43255030ed07023764
Mafra Mano <maframano@hotmail.com>
2020-03-26 10:57:00 -0300

    Login de usuario negativo.Sem thread sleep

--- ProjetoTDD/src/test/java/PageLoggin/PaginaDeLogin.java
+++ ProjetoTDD/src/test/java/PageLoggin/PaginaDeLogin.java
@@ -2,12 +2,16 @@ package PageLoggin;
 
 import static org.testng.Assert.assertEquals;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 //import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Negocio.BasePages;
@@ -38,7 +42,7 @@ public class PaginaDeLogin extends BasePages{
 	
 	public PaginaDeLogin preencheDadosUsuarioNegativo() throws InterruptedException{ 
 
-	navegador.findElement(By.name("username")).sendKeys("MafraMano");
+	navegador.findElement(By.name("username")).sendKeys("benedito");
 	navegador.findElement(By.name("password")).sendKeys("4Jfkdhfkhs");
 	
 	navegador.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -47,20 +51,28 @@ public class PaginaDeLogin extends BasePages{
 	
 	navegador.findElement(By.id("sign_in_btnundefined")).click();
 
+	// FluentWait wait = new FluentWait(navegador);
+	// wait.withTimeout(5000, TimeUnit.MILLISECONDS);
+	// wait.pollingEvery(250, TimeUnit.MILLISECONDS);
+	// wait.ignoring(NoSuchElementException.class);
+	// wait.until(ExpectedConditions.alertIsPresent());
 	
-	
-	 Thread.sleep(500);
+	//Thread.sleep(500);
 	//Verificar mensagem de erro no top
+	WebDriverWait aguardar = new WebDriverWait(navegador, Duration.ofSeconds(10));
 	WebElement me = navegador.findElement(By.id("signInResultMessage"));
+	//wait.until(ExpectedConditions.textToBePresentInElement(validacao, "Incorrect user name or password."));
+	aguardar.until(ExpectedConditions.textToBePresentInElement(me, "Incorrect user name or password."));
+
 		String actual = me.getText();
 		assertEquals("Incorrect user name or password.", actual);
-		
 	
 	return this;
 	
 	}
 }
 
+//elementToBeClickable
 
-
+//wait.until(ExpectedConditions.textToBePresentInElement(validacao, "Incorrect user name or password."));
 //assertEquals("User is temporary blocked from login", actual);
